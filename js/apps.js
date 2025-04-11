3b5281ad9d43324a2468efdf7e803cf3ca534030
doston <dostonadxamov@222gmail.com>
2025-04-11 20:26:32 +0500

    initial commit

---
+++ js/apps.js
@@ -0,0 +1,161 @@
+
+
+// ==============
+// 1 masala
+// ==============
+
+// const full =false;
+// while (! full) {
+//     const randomNumber =Math.round(Math.random() * 77);
+//     console.log(randomNumber);
+//     const check =prompt("siz oylagan son shumi");
+//     if (check === "ha") {
+//         console.log(`${randomNumber} soni oyladingiz`);
+//     }
+// }
+
+
+
+
+// ==============
+// 1 masala
+// ==============
+
+// const a = prompt("son kiriiing");
+
+// console.log("floor", Math.floor(a));
+// console.log("round", Math.round(a));
+// console.log("ceil", Math.ceil(a));
+
+
+
+
+// ==============
+// 2 masala
+// ==============
+
+// const a = Math.round(Math.random() * 10);
+
+// console.log("random son" ,a);
+
+
+
+
+// ==============
+// 3 masala
+// ==============
+
+// const nul = "";
+// let undefined;
+
+// console.log("null -- bu qiymat yoqligini belgilash uchun ishlatamz");
+// console.log("undefined -- bunda hali qiymat berilmagan ozgaruvchi deb tushunaman", undefined);
+
+// ==============
+// 4 masala
+// ==============
+
+// let a = 0;
+
+// while (a < 10) {
+//     const randomNumber = Math.round(Math.random() * 100);
+//     console.log(randomNumber);
+//     a++;
+// }
+
+
+// ==============
+// 5 masala
+// ==============
+
+// const a = prompt("son kiriting");
+
+// console.log(`${a} ning kvadrati`, Math.pow(a, 2));
+
+
+// ==============
+// 6 masala
+// ==============
+
+// let counter = 0;
+
+// while (counter <=20) {
+//     counter = counter + 1;
+
+    
+//     if (counter % 2){
+//         continue;
+//     }
+//     console.log(counter);
+// }
+
+
+// ==============
+// 7 masala
+// ==============
+
+// const a = Math.round(Math.random) * 10;
+// let counter = 0;
+// do {
+//     const b = prompt("son kiriting");
+//     counter = counter + 1;
+//     if (b == 5) {
+//        console.log(`togri topdingiz ${b}`);
+//        break;
+//     }
+//     else{
+//         console.log("xato, yana kiritng");
+//     }
+// }while(counter <=6);
+    
+
+
+// } while (counter <= 5);
+
+// ==============
+// 8 masala
+// ==============
+
+// const a = 0;
+// while (a <= 50) {
+//     if (a % 3 == 0) {
+//         console.log(a);
+//     }
+//     a = a + 1;
+    
+// }
+
+
+// ==============
+// 9 masala
+// ==============
+
+// const  a1 = Math.round(Math.random() * 100);
+// const  a2 = Math.round(Math.random() * 100);
+// const  a3 = Math.round(Math.random() * 100);
+// const  a4 = Math.round(Math.random() * 100);
+// const  a5 = Math.round(Math.random() * 100);
+
+// console.log("sonlar", a1, a2, a3, a4, a5);
+
+// if (a1 > a2 && a1 > a3 && a1 > a4 && a1 > a5) {
+//     console.log("eng katta son", a1);
+// }
+// else if (a2 > a1 && a2 > a3 && a2 > a4 && a2 > a5) {
+//     console.log("eng katta son", a2);
+// } else if (a3 > a1 && a3 > a2 && a3 > a4 && a3 > a5) {
+//     console.log("eng katta son", a3);
+// } else if (a4 > a1 && a4 > a2 && a4 > a3 && a4 > a5) {
+//     console.log("eng katta son", a4);
+// } else {
+//     console.log("eng katta son", a5);
+// }
+
+// ==============
+// 10 masala
+// ==============
+
+// const son = prompt("son kiriting");
+// if (son == "" || son == 0) {
+//     console.log("iltimos son kiriting");
+// }
